b1f392d6134aef5ef4824f29e6b921b12430144f
İrem <71925132+yilmaziremnur@users.noreply.github.com>
2022-11-10 15:27:47 +0300

    Create Query.sql

---
+++ Query.sql
@@ -0,0 +1,13 @@
+use Databasename
+CREATE TABLE Tablename
+(
+Id int identity(1,1) primary key,
+il varchar(50),
+sube varchar(50),
+personelsayi int,
+urunsayi int,
+stokkodu int
+)
+INSERT INTO Tablename
+VALUES ('Value1','Value2',Value3,Value4)
+SELECT *FROM Tablename
